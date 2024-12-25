089462b3d8b49bb6fefed4fb65bdad1805be5d3e
Onhalar <halaczondrej@gmail.com>
2024-12-25 16:20:05 +0100

    first commit

---
+++ src/TasksApp.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import Utils, DateTime
+
+def main( title:str = "TasksApp" ):
+    main = Tk()
+    main.title("TasksApp")
+    main.resizable(False, False)
+
+    main.mainloop()
